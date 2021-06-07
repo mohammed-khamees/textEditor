13acae923fa7d783b44f9331ecc898f35dafd4e0
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-06-07 20:10:00 +0300

    fixed a bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 const app = express();
-const port = 3500;
+const port = 3030;
 
 const server = http.createServer(app);
 const io = socket(server);
